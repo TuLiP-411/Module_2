dcee6bb2ef63b4016816f34996be5f457dc3d9d8
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-08-09 13:42:13 +0700

    Add files via upload

---
+++ Rectangle/src/com/codegym/Main.java
@@ -0,0 +1,18 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.println("Enter the height: ");
+        double height = scanner.nextDouble();
+        Rectangle rec = new Rectangle(width, height);
+        System.out.println("Your Rectangle \n" + rec.display());
+        System.out.println("Perimeter of the rectangle: " + rec.getPerimeter());
+        System.out.println("Area of the rectangle: " + rec.getArea());
+    }
+}
