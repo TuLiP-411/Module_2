09753067e733eb935eb251e786dde47b03e3410c
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-08-11 15:15:35 +0700

    Add files via upload

---
+++ triangle-inheritance-class/src/com/codegym/Test.java
@@ -0,0 +1,10 @@
+package com.codegym;
+
+public class Test {
+    public static void main(String[] args) {
+        Triangle tri1 = new Triangle();
+        tri1.setSide(5, 4, 3);
+        tri1.setColor("baby blue");
+        System.out.println(tri1.toString());
+    }
+}
