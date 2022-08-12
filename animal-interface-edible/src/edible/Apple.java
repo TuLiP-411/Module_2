f5a14da593881f2e335a0019b84cff1b5b89e35f
Tu Linh Phung <65971089+TuLiP-411@users.noreply.github.com>
2022-08-12 14:24:48 +0700

    Add files via upload

---
+++ animal-interface-edible/src/edible/Apple.java
@@ -0,0 +1,8 @@
+package edible;
+
+public class Apple extends Fruit {
+    @Override
+    public String howToEat() {
+        return "slice when ripe, eat raw";
+    }
+}
---
+++ animal-interface-edible/src/edible/Orange.java
@@ -0,0 +1,8 @@
+package edible;
+
+public class Orange extends Fruit {
+    @Override
+    public String howToEat() {
+        return "Slice to eat raw, or squeeze to get juice";
+    }
+}
